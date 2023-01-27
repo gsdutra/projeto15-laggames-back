d1842bd335359714d40c2800dd25b595613cf866
andreprogramador01 <andreprogramador01@gmail.com>
2023-01-27 19:43:11 -0300

    feat:login do usuario back

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import schemaValidation from "../middlewares/schemaValidationMiddleware.js";
 import cadastroSchema from "../schema/cadastroSchema.js";
 import { Router } from "express";
-import { cadastro } from "../controller/authController.js";
+import { cadastro,login } from "../controller/authController.js";
+
+import loginSchema from "../schema/loginSchema.js";
 
 const authRoutes = Router()
 
 authRoutes.post('/cadastro', schemaValidation(cadastroSchema), cadastro)
+authRoutes.post('/login', schemaValidation(loginSchema), login)
 
 export default authRoutes
