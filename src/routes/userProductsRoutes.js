85360854cb16f903f8957b61b191bc7030235825
gsdutra <gabrielsprecherdutra@gmail.com>
2023-01-29 03:19:10 -0300

    feat: product ammount

--- src/routes/userProductsRoutes.js
+++ src/routes/userProductsRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUserProducts, postUserProducts, deleteUserProducts } from '../controller/userProducts.js'
+import { getUserProducts, postUserProducts, deleteUserProducts, putUserProducts } from '../controller/userProducts.js'
 import { verificaToken } from '../middlewares/verificaToken.js'
 
 const userProductsRoutes = Router()
@@ -7,5 +7,6 @@ const userProductsRoutes = Router()
 userProductsRoutes.get("/userProducts", verificaToken, getUserProducts)
 userProductsRoutes.post("/userProducts", verificaToken, postUserProducts)
 userProductsRoutes.delete("/userProducts/:productId", verificaToken, deleteUserProducts)
+userProductsRoutes.put("/userProducts/:productId", verificaToken, putUserProducts)
 
 export default userProductsRoutes
